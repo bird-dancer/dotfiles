8b4c7c43b15c05e9a20f16930cb3eea3e1e45d28
Felix <f.dumbeck@campus.tu-berlin.de>
2023-02-19 15:51:54 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-source "$HOME/.zshrc"
+stow .
+
+if [ "$ZSH_VERSION" ]; then
+    autoload -U compinit
+    compinit
+fi
+
 sudo localectl set-x11-keymap de
 
 echo "finished"
