65da6091475a2768960209852274debd13a7eddd
Felix <53702818+f-eliks@users.noreply.github.com>
2023-05-02 00:53:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,6 @@
 stow --adopt .
 git reset --hard
 
-"$HOME/.config/emacs.d/bin/doom" sync -u
-"$HOME/.config/emacs.d/bin/doom" env
-
-#sudo localectl set-x12-keymap de
+#sudo localectl set-x11-keymap de
 
 echo "finished now just run source $HOME/.zshrc"
