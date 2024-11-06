1ae57944036ff6405dc5e14f8db9effd22e056e8
Felix Dumbeck <felix@dumbeck.net>
2024-11-06 22:38:48 +0100

    emacs: add magit-todos, php-mode, c kernel style

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -499,6 +499,10 @@
   :bind (("C-x g" . magit)
          ("C-x c" . magit-clone-shallow)))
 
+(use-package magit-todos
+  :after magit
+  :config (magit-todos-mode 1))
+
 (use-package forge
   :after magit)
 
@@ -520,6 +524,8 @@
   :mode ("\\.md\\'" . markdown-mode)
   :config (setq markdown-command "multimarkdown"))
 
+(use-package php-mode)
+
 (use-package web-mode
   :defer t
   :config
@@ -535,10 +541,10 @@
 (add-to-list 'auto-mode-alist '("\\Makefile\\'" . makefile-mode))
 
 (require 'c-ts-mode)
-(add-hook 'c-ts-mode-hook (c-ts-mode-set-global-style 'linux))
-(when (eq c-ts-mode-indent-style 'linux)
-  (setq c-ts-mode-indent-offset 8)
-  (setq comment-style 'extra-line))
+(add-hook 'c-ts-mode-hook (lambda () (c-ts-mode-set-global-style 'linux)
+                            (when (eq c-ts-mode-indent-style 'linux)
+                              (setq c-ts-mode-indent-offset 8)
+                              (setq comment-style 'extra-line))))
 
 ;; (define-key flymake-mode-map (kbd "M-n") 'flymake-goto-next-error)
 ;; (define-key flymake-mode-map (kbd "M-p") 'flymake-goto-prev-error)
