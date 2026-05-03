02667aa3dcbe37d4332d6652136d00692e11b9a4
Felix Dumbeck <felix@dumbeck.net>
2026-05-03 23:46:27 +0200

    nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -9,12 +9,12 @@
   let
     pkgs = nixpkgs.legacyPackages.x86_64-linux;
   in {
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
     packages.x86_64-linux.default = pkgs.buildEnv {
       name = "my-packages";
       paths = with pkgs; [
         gleam
         erlang
+	rebar3
 	pwntools
 	vbindiff
       ];
